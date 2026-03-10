f40d9730a32acdb5a2eda2871e475ee125ecf0de
Sureshkumar V <drop2vsk@gmail.com>
2026-03-10 21:11:25 +0530

    Validation Method intro

--- banking_cli_app/services/bank_service.py
+++ banking_cli_app/services/bank_service.py
@@ -6,11 +6,30 @@ class Bankservice():
         self.users = {}
         self.current_user = None
 
+    def _get_validation_account(self,account_number):
+        if self.current_user is None:
+            raise ValueError("User must logged in")
+        if account_number not in self.accounts:
+            raise ValueError("Account number is not exist")
+        account = self.accounts[account_number]
+        if account not in self.current_user.accounts:
+            raise ValueError("This account number is not belongs to the current user")
+        return account
+
     def create_user(self,user_name,password):
         if user_name in self.users:
             raise ValueError("User is already exists") 
         user = User(user_name,password)
         self.users[user_name] = user
+        return user
+    
+    def user_login(self,user_name,password):
+        if user_name not in self.users:
+            raise ValueError ("User does not exist")
+        user = self.users[user_name]
+        if password != user.password:
+            raise ValueError ("Wrong Password")
+        self.current_user = user
         return user
 
     def create_account(self,account_number):
@@ -23,19 +42,14 @@ class Bankservice():
         self.accounts[account_number] = account
         return account
 
-
     def deposit(self,account_number,amount):
-        pass
+        account = self._get_validation_account(account_number)
+        return account.deposit(amount)
+    
     def withdraw(self,account_number,amount):
-        pass
-    def user_login(self,user_name,password):
-        if user_name not in self.users:
-            raise ValueError ("User does not exist")
-        user = self.users[user_name]
-        if password != user.password:
-            raise ValueError ("Wrong Password")
-        self.current_user = user
-        return user
+        account = self._get_validation_account(account_number)
+        return account.withdraw(amount)
+
     def user_logout(self):
         pass
     def get_account(self,account_number):
